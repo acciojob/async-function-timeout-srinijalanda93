b46e7a9b00c9c8214ad3ba76f700f961a7f9f76b
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-25 13:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.getElementById("btn").addEventListener("click",async ()=>{
-	const text=document.getElementById("text").value;
-	const delay=document.getElementById("delay").value;
-	//create a Promise Class where it take the text in delay ms
-	if(text && delay){
-	let p=new Promise((resolve,reject) =>{
-		setTimeOut(()=>{
-			resolve(text)
-		},delay);
-	})
-	//using await keyword
-		let text2=await p;
-		if(text2){
-			document.getElmentById("output").textContent=text2;
-		}
-	}
-	
-	
-})
+document.getElementById("btn").addEventListener("click", async () => {
+  const text = document.getElementById("text").value;
+  const delay = document.getElementById("delay").value;
+
+  // create a Promise Class where it takes the text in delay ms
+  if (text && delay) {
+    let p = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(text);
+      }, delay);
+    });
+
+    // using await keyword
+    let text2 = await p;
+    if (text2) {
+      document.getElementById("output").textContent = text2;
+    }
+  }
+});
