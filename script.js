5765ed2466ee4f2fedfa19ca7c246af2e4e7b27c
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-25 13:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async ()=>{
+	const text=document.getElementById("text").value;
+	const delay=document.getElementById("delay").value;
+	//create a Promise Class where it take the text in delay ms
+	if(text && delay){
+	let pro=new Promise((reslove,reject) =>{
+		setTimeOut(()=>{
+			resolve(text)
+		},delay);
+	})
+	//using await keyword
+		let text2=await pro;
+		if(text2){
+			document.getElmentById("output").textContent=text2;
+		}
+	}
+	
+	
+})
